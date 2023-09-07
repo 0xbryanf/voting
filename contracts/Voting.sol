91ab244d3a2dbf67318566983585c64b17849765
0xbryanf <0xbryanf@gmail.com>
2023-09-07 16:59:54 +0800

    modified Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -15,10 +15,9 @@ contract Voting {
         uint256 voteCount; // number of accumulated votes
     }
 
-    address immutable chairperson;
+    address chairperson;
 
     mapping(address => Voter) public voters;
-    mapping(address => bool) public voterStatus;
 
     Proposal[] public proposals;
 
@@ -38,13 +37,55 @@ contract Voting {
         require(msg.sender == chairperson, "Only chairperson can give right to vote.");
         require(!voters[voter].voted, "The voter already voted.");
         require(voters[voter].weight == 0, "The voter's weight must be 0");
-        require(!voterStatus[voter], "The voter status must not be true.");
 
         voters[voter].weight = 1;
-        voterStatus[voter] = true;
     }
 
-    
+    function delegate(address to) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "You have no right to vote." );
+        require(!sender.voted, "You already voted");
+        require(to != msg.sender, "Self-delegation is disallowed.");
+        
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+            require(to != msg.sender, "Found loop in delegation");
+        }
 
+        Voter storage delegate_ = voters[to];
 
+        require(delegate_.weight >= 1);
+
+        sender.voted = true;
+        sender.delegate = to;
+
+        if (delegate_.voted) {
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            delegate_.weight += sender.weight;
+        }
+    }
+
+    function vote(uint proposal) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
 }
